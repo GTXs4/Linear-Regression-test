0f8da6c16fb03693d4c3b5e29cda222532d0614e
GTXs <arayabarrioseduardo@gmail.com>
2020-05-28 17:57:51 -0400

    Puliendo un poco la documentacion

--- methods.py
+++ methods.py
@@ -1,6 +1,69 @@
 
 class linear_regression:
     
+    """
+    Crea un objeto que realiza una regresion lineal. Puedes a traves de este obtener 
+    los coeficientes respectivos y el coeficiente de determinacion.
+    
+    Parametros
+    ---------
+    :parametro *args: Estos son los regresores. Cada regresor entregado debe ser una lista.
+    :parametro y: (list) Esta es la respuesta observada. 
+    
+    Atributos
+    ---------
+    :X: (numpy.matrix) Esta es la matriz de regresores
+    :Y: (numpy.matrix) Esta es la matriz de respuesta
+    :k: (int) Numero de regresores
+    :n: (int) Numero de datos o filas
+    :coef: (numpy.matrix) Coeficientes de la regresion lineal
+    :R2: (float) Coeficiente de determinacion
+    :R2_adj: (float) Coeficiente de determinacion ajustado
+    
+    Metodos
+    -------
+    :resumen(): Te imprime en pantalla un resumen de la regresion lineal
+    :prediccion_(x_pred): (numpy.float64) Retorna una prediccion segun los valores de los regresores que se le ingrese
+    
+    Notas
+    -----
+    Esta clase puede resolver una regresion lineal simple
+    
+    .. math:: y = α + β x + \epsilon
+    
+    Pero tambien puede resolver una regresion lineal multiple
+    
+    .. math:: y = {β}_0 +{β}_1 {x}_1 +{β}_2 {x}_2 + \cdots +{β}_n {x}_n + \epsilon
+           
+    
+    Ejemplos
+    --------
+    Ejemplo de regresion lineal simple
+    
+    >>> x = [1.3, 3.5, 4.2, 8.0]
+    >>> y = [4.1, 8.4, 10.14, 19.1]
+    >>> lm1 = linear_regression(x,y=y)
+    >>> lm1.resumen()
+    Coeficiente 0 : 0.83051
+    Coeficiente 1 : 2.25988
+    R2: 0.99753
+    R2_ajustado: 0.99629
+    
+    Ejemplo de regresion lineal multiple
+    
+    >>> x1 = [2.3, 3.1, 4.6, 6.4]
+    >>> x2 = [3.4, 9.1, 11.3, 12.3]
+    >>> y = [15.2, 33.87, 45.04, 50.24]
+    >>> lm2 = linear_regression(x1,x2,y=y)
+    >>> lm2.resumen()
+    Coeficiente 0 : 0.16005
+    Coeficiente 1 : 1.81865
+    Coeficiente 2 : 3.15468
+    R2: 0.9982
+    R2_ajustado: 0.99459
+    
+    """
+    
     def __init__(self,*args,y):
         self.X, self.Y = self.crear_matrices(*args,y=y)
         self.k = len(args)
@@ -10,6 +73,9 @@ class linear_regression:
         self.R2_adj = self.R2_ajust_()
     
     def resumen(self):
+        """
+        Imprime un resumen de la regresion lineal
+        """
         for i,coeficiente in enumerate(self.coef):
             print("Coeficiente {} : {}".format(i, round(coeficiente.item(0),5)))
         print("R2: {}".format(round(self.R2,5)))
@@ -17,12 +83,16 @@ class linear_regression:
     
     def crear_matrices(self,*args,y):
         """    
-        Devuelve las matriz X para los regresores y la matriz Y para la respuesta
+        Crea las matriz X para los regresores y la matriz Y para la respuesta
 
         PARAMETROS
-        ---------------------------------
-        *args: estos son los regresores
-        y: esta es la respuesta observada
+        ----------
+        :*args: estos son los regresores
+        :y: esta es la respuesta observada
+        
+        RETORNO
+        -------
+        :X,Y: (numpy.matrix) Devuelve las matriz X para los regresores y la matriz Y para la respuesta
         """
         import numpy as np
         n = len(y)
@@ -41,12 +111,17 @@ class linear_regression:
         return X, Y 
     
     def calcular_b(self,X,Y):
-        """Devuelve los coeficientes para la regresion lineal
+        """
+        Calcula los coeficientes para la regresion lineal
 
         PARAMETROS
-        ---------------------------------
-        X: Esta es la matriz de regresores
-        Y: Esta es la matriz de la respuesta observada
+        ----------
+        :X: Esta es la matriz de regresores
+        :Y: Esta es la matriz de la respuesta observada
+        
+        RETORNO
+        -------
+        :b: (numpy.matrix) Devuelve los coeficientes para la regresion lineal
         """ 
         import numpy as np
         XX = X*X.transpose()
@@ -67,13 +142,14 @@ class linear_regression:
         return 1-(numerador/denominador)
     
     def SCE_matricial(self,X,Y,n):
-        """Retorna SCE
+        """
+        Retorna SCE
 
         PARAMETROS
-        ---------------------------------
-        X: Esta es la matriz de regresores
-        Y: Esta es la matriz de la respuesta observada
-        n: Es el numero de datos
+        ----------
+        :X: Esta es la matriz de regresores
+        :Y: Esta es la matriz de la respuesta observada
+        :n: Es el numero de datos
         """
         import numpy as np
         XX = X*X.transpose()
@@ -84,13 +160,14 @@ class linear_regression:
         return SCE.item(0)
     
     def SCR_matricial(self,X,Y,n):
-        """Retorna SCR
+        """
+        Retorna SCR
 
         PARAMETROS
-        ---------------------------------
-        X: Esta es la matriz de regresores
-        Y: Esta es la matriz de la respuesta observada
-        n: Es el numero de datos
+        ----------
+        :X: Esta es la matriz de regresores
+        :Y: Esta es la matriz de la respuesta observada
+        :n: Es el numero de datos
         """
         import numpy as np
         XX = X*X.transpose()
@@ -106,12 +183,13 @@ class linear_regression:
         return SCR
     
     def STCC_matricial(self,Y,n):
-        """Retorna STCC
+        """
+        Retorna STCC
 
         PARAMETROS
         ---------------------------------
-        X: Esta es la matriz de regresores
-        n: Es el numero de datos
+        :X: Esta es la matriz de regresores
+        :n: Es el numero de datos
         """
         import numpy as np
         ones = np.ones(n)
@@ -123,11 +201,18 @@ class linear_regression:
         return STCC
    
     def prediccion_(self,x_pred):
-        """ Retorna un valor de y estimado
+        """ 
+        Predice un valor segun los valores de los regresores ingresados
         
         PARAMETROS
-        ---------------------------------
-        x_pred: Es una lista con los valores de x1,x2,..etc para hacer la estimacion de y """
+        ----------
+        :x_pred: Es una lista con los valores de x1,x2,..etc para hacer la estimacion de y 
+        
+        RETORNO
+        -------
+        (numpy.float64) Retorna un valor de y estimado
+        
+        """
         import numpy as np
         largo = len(self.coef)
         x_pred.insert(0,1.0)
