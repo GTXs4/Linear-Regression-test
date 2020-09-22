29716720a8715b4c48d2f06cc3545d5ab5cf2c66
GTXs <arayabarrioseduardo@gmail.com>
2020-09-21 23:23:33 -0300

    Modifying documentation and import

--- main.py
+++ main.py
@@ -5,70 +5,70 @@ import pandas as pd
 import numpy as np
 from tabulate import tabulate
 
-#DATOS DE PRUEBA
+#TEST DATA
 np.random.seed(12)
 n=13
-datos = pd.DataFrame({"x1":5.1 + 5.0*np.random.randn(n),
+data = pd.DataFrame({"x1":5.1 + 5.0*np.random.randn(n),
                       "x2":1.3 + 3.2*np.random.randn(n),
                       "x3":3.7 + 1.3*np.random.randn(n),
                       "y":25.2 + 12.3*np.random.randn(n)})
 
-x1 = datos["x1"].tolist()
-x2 = datos["x2"].tolist()
-x3 = datos["x3"].tolist()
-y = datos["y"].tolist()
+x1 = data["x1"].tolist()
+x2 = data["x2"].tolist()
+x3 = data["x3"].tolist()
+y = data["y"].tolist()
 
 if __name__ == "__main__":
     #df.head()
-    interceptos = []
+    intercepts = []
     coef_x1 = []
     coef_x2 = []
     coef_x3 = []
     R2s = []
-    predicciones = []
+    predictions = []
     x_pred = [2.5, 7.20, 9.12]
     X_pred = pd.DataFrame({"x1":[x_pred[0]],"x2":[x_pred[1]],"x3":[x_pred[2]]})
     
-    #IMPLEMENTACION PROPIA
+    #OWN IMPLEMENTATION
     lm1 = linear_regression(x1,x2,x3,y=y)
-    print("Resumen Implementacion\n")
-    lm1.resumen()
+    print("Implementation Summary\n")
+    lm1.summary()
         
-    interceptos.append(round(lm1.coef.item(0),5))
+    intercepts.append(round(lm1.coef.item(0),5))
     coef_x1.append(round(lm1.coef.item(1),5))
     coef_x2.append(round(lm1.coef.item(2),5))
     coef_x3.append(round(lm1.coef.item(3),5))
     R2s.append(round(lm1.R2,5))
-    predicciones.append(round(lm1.prediccion_(x_pred),5))
+    predictions.append(round(lm1.prediction_(x_pred),5))
           
-    X = datos[["x1","x2","x3"]]
-    Y = datos["y"]
+    X = data[["x1","x2","x3"]]
+    Y = data["y"]
     
     #SCIKIT LEARN
     lm2 = LinearRegression().fit(X,Y)
     
-    interceptos.append(round(lm2.intercept_,5))
+    intercepts.append(round(lm2.intercept_,5))
     coef_x1.append(round(lm2.coef_[0],5))
     coef_x2.append(round(lm2.coef_[1],5))
     coef_x3.append(round(lm2.coef_[2],5))
     R2s.append(round(lm2.score(X,Y),5))
-    predicciones.append(round(lm2.predict(X_pred).item(0),5))
+    predictions.append(round(lm2.predict(X_pred).item(0),5))
     
     #STATSMODELS
-    lm3 = smf.ols(formula = "y ~ x1 + x2 + x3", data = datos).fit()
-    interceptos.append(round(lm3.params[0],5))
+    lm3 = smf.ols(formula = "y ~ x1 + x2 + x3", data = data).fit()
+    intercepts.append(round(lm3.params[0],5))
     coef_x1.append(round(lm3.params[1],5))
     coef_x2.append(round(lm3.params[2],5))
     coef_x3.append(round(lm3.params[3],5))
     R2s.append(round(lm3.rsquared,5))
-    predicciones.append(round(lm3.predict(X_pred),5))
+    predictions.append(round(lm3.predict(X_pred),5))
     
     #print(lm3.rsquared_adj)
-    print("\nComparacion de los resultados de los modelos generados con diferentes implementaciones \n")
-    nombres = ["implementacion","scikit","statmodels"]
+    print("\nComparison of the results of the models generated with different implementations \n")
+    names = ["implementation","scikit","statmodels"]
     table = []
-    for i in range(len(nombres)):
-        table.append([nombres[i], interceptos[i], coef_x1[i], coef_x2[i], coef_x3[i], R2s[i], predicciones[i]])
-    print(tabulate(table, headers=["Modelo","intercepto","coef_x1","coef_x2","coef_x3","R2","prediccion"]))
+    for i in range(len(names)):
+        table.append([names[i], intercepts[i], coef_x1[i], coef_x2[i], coef_x3[i], R2s[i], predictions[i]])
+    print(tabulate(table, headers=["Model","intercept","coef_x1","coef_x2","coef_x3","R2","prediction"]))
     
     
--- methods.py
+++ methods.py
@@ -1,100 +1,126 @@
+import numpy as np
 
 class linear_regression:
     
     """
-    Crea un objeto que realiza una regresion lineal. Puedes a traves de este obtener 
-    los coeficientes respectivos y el coeficiente de determinacion.
+    Create an object that performs a linear regression. You can through this obtain 
+    the respective coefficients and the coefficient of determination.
     
-    Parametros
+    Parameters
     ---------
-    :parametro *args: Estos son los regresores. Cada regresor entregado debe ser una lista.
-    :parametro y: (list) Esta es la respuesta observada. 
+    parameter *args : variable number of arguments 
+        These are the regressors. Each returned regressor must be a list.
     
-    Atributos
+    parameter y : list
+        This is the observed response.
+    
+    Attributes
     ---------
-    :X: (numpy.matrix) Esta es la matriz de regresores
-    :Y: (numpy.matrix) Esta es la matriz de respuesta
-    :k: (int) Numero de regresores
-    :n: (int) Numero de datos o filas
-    :coef: (numpy.matrix) Coeficientes de la regresion lineal
-    :R2: (float) Coeficiente de determinacion
-    :R2_adj: (float) Coeficiente de determinacion ajustado
+    X : numpy.matrix
+        This is the regressor matrix
+        
+    Y : numpy.matrix 
+        This is the response matrix
     
-    Metodos
+    k : int 
+        Number of regressors
+    
+    n : int 
+        Number of data or rows
+    
+    coef : numpy.matrix 
+        Coefficients of linear regression
+    
+    R2 : float 
+        Determination coefficient
+    
+    R2_adj : float 
+        Adjusted coefficient of determination
+    
+    Methods
     -------
-    :resumen(): Te imprime en pantalla un resumen de la regresion lineal
-    :prediccion_(x_pred): (numpy.float64) Retorna una prediccion segun los valores de los regresores que se le ingrese
+    summary() : None
+        Print a summary of the linear regression on the screen
     
-    Notas
+    prediction_(x_pred) : numpy.float64 
+        Returns a prediction according to the values of the regressors entered
+    
+    Notes
     -----
-    Esta clase puede resolver una regresion lineal simple
+    This class can solve a simple linear regression
     
     .. math:: y = α + β x + \epsilon
     
-    Pero tambien puede resolver una regresion lineal multiple
+    But you can also solve a multiple linear regression
     
     .. math:: y = {β}_0 +{β}_1 {x}_1 +{β}_2 {x}_2 + \cdots +{β}_n {x}_n + \epsilon
            
     
-    Ejemplos
+    Examples
     --------
-    Ejemplo de regresion lineal simple
+    Simple linear regression example
     
     >>> x = [1.3, 3.5, 4.2, 8.0]
     >>> y = [4.1, 8.4, 10.14, 19.1]
     >>> lm1 = linear_regression(x,y=y)
-    >>> lm1.resumen()
-    Coeficiente 0 : 0.83051
-    Coeficiente 1 : 2.25988
+    >>> lm1.summary()
+    Coefficient 0 : 0.83051
+    Coefficient 1 : 2.25988
     R2: 0.99753
-    R2_ajustado: 0.99629
+    R2_adjusted: 0.99629
     
-    Ejemplo de regresion lineal multiple
+    Multiple linear regression example
     
     >>> x1 = [2.3, 3.1, 4.6, 6.4]
     >>> x2 = [3.4, 9.1, 11.3, 12.3]
     >>> y = [15.2, 33.87, 45.04, 50.24]
     >>> lm2 = linear_regression(x1,x2,y=y)
-    >>> lm2.resumen()
-    Coeficiente 0 : 0.16005
-    Coeficiente 1 : 1.81865
-    Coeficiente 2 : 3.15468
+    >>> lm2.summary()
+    Coefficient 0 : 0.16005
+    Coefficient 1 : 1.81865
+    Coefficient 2 : 3.15468
     R2: 0.9982
-    R2_ajustado: 0.99459
+    R2_adjusted: 0.99459
     
     """
     
     def __init__(self,*args,y):
-        self.X, self.Y = self.crear_matrices(*args,y=y)
+        self.X, self.Y = self.create_matrix(*args,y=y)
         self.k = len(args)
         self.n = len(y)
-        self.coef = self.calcular_b(self.X,self.Y)
+        self.coef = self.calculate_b(self.X,self.Y)
         self.R2 = self.R2_()
-        self.R2_adj = self.R2_ajust_()
+        self.R2_adj = self.R2_adjust_()
     
-    def resumen(self):
+    def summary(self):
         """
-        Imprime un resumen de la regresion lineal
+        Print a summary of the linear regression
         """
-        for i,coeficiente in enumerate(self.coef):
-            print("Coeficiente {} : {}".format(i, round(coeficiente.item(0),5)))
-        print("R2: {}".format(round(self.R2,5)))
-        print("R2_ajustado: {}".format(round(self.R2_adj,5)))
-    
-    def crear_matrices(self,*args,y):
-        """    
-        Crea las matriz X para los regresores y la matriz Y para la respuesta
-
-        PARAMETROS
-        ----------
-        :*args: estos son los regresores
-        :y: esta es la respuesta observada
         
-        RETORNO
+        for i,coef in enumerate(self.coef):
+            print("Coefficient {} : {}".format(i, round(coef.item(0),5)))
+        print("R2: {}".format(round(self.R2,5)))
+        print("R2_adjusted: {}".format(round(self.R2_adj,5)))
+    
+    def create_matrix(self,*args,y):
+        """    
+        Create the X matrix for the regressors and the Y matrix for the answer
+
+        Parameters
+        ----------
+        *args : variable number of lists
+            these are the regressors
+        
+        y : list
+            this is the observed response
+        
+        Returns
         -------
-        :X,Y: (numpy.matrix) Devuelve las matriz X para los regresores y la matriz Y para la respuesta
+        X,Y : numpy.matrix 
+            Returns the X matrix for the regressors and the Y matrix for the answer
+        
         """
-        import numpy as np
+        
         n = len(y)
         k = len(args)
         ones = np.ones(n)
@@ -110,113 +136,136 @@ class linear_regression:
         Y = np.matrix([y])
         return X, Y 
     
-    def calcular_b(self,X,Y):
+    def calculate_b(self,X,Y):
         """
-        Calcula los coeficientes para la regresion lineal
+        Calculate the coefficients for linear regression
 
-        PARAMETROS
+        Parameters
         ----------
-        :X: Esta es la matriz de regresores
-        :Y: Esta es la matriz de la respuesta observada
+        X : numpy.matrix
+            This is the regressor matrix
         
-        RETORNO
+        Y : numpy.matrix
+            This is the matrix of the observed response
+        
+        Returns
         -------
-        :b: (numpy.matrix) Devuelve los coeficientes para la regresion lineal
+        b : numpy.matrix 
+            Returns the coefficients for linear regression
+        
         """ 
-        import numpy as np
+        
         XX = X*X.transpose()
         inverseXX = np.linalg.inv(XX)
         b = inverseXX * X * Y.transpose()
         return b
     
     def R2_(self):
-        """Retorna el coeficiente de determinacion R2"""
-        import numpy as np
-        return (self.SCR_matricial(self.X,self.Y,self.n)/self.STCC_matricial(self.Y,self.n))
+        """Returns the coefficient of determination R2"""
+        
+        return (self.SCR_matrix(self.X,self.Y,self.n)/self.STCC_matrix(self.Y,self.n))
     
-    def R2_ajust_(self):
-        """Retorna el coeficiente de determinacion ajustado R2_adjusted"""
-        import numpy as np
-        numerador = self.SCE_matricial(self.X,self.Y,self.n)/(self.n-self.k-1)
-        denominador = self.STCC_matricial(self.Y,self.n)/(self.n-1)
-        return 1-(numerador/denominador)
+    def R2_adjust_(self):
+        """Returns the adjusted coefficient of determination R2_adjusted"""
+        
+        numerator = self.SCE_matrix(self.X,self.Y,self.n)/(self.n-self.k-1)
+        denominator = self.STCC_matrix(self.Y,self.n)/(self.n-1)
+        return 1-(numerator/denominator)
     
-    def SCE_matricial(self,X,Y,n):
+    def SCE_matrix(self,X,Y,n):
         """
-        Retorna SCE
+        Returns SCE
 
-        PARAMETROS
+        Parameters
         ----------
-        :X: Esta es la matriz de regresores
-        :Y: Esta es la matriz de la respuesta observada
-        :n: Es el numero de datos
+        X : numpy.matrix 
+            This is the regressor matrix
+        
+        Y : numpy.matrix 
+            This is the matrix of the observed response
+        
+        n : int
+            It is the number of data
+        
         """
-        import numpy as np
+        
         XX = X*X.transpose()
         inverseXX = np.linalg.inv(XX)
 
-        identidad = np.identity(n)
-        SCE = Y *(identidad - ((X.transpose())*inverseXX*X) ) * Y.transpose()
+        identity = np.identity(n)
+        SCE = Y *(identity - ((X.transpose())*inverseXX*X) ) * Y.transpose()
         return SCE.item(0)
     
-    def SCR_matricial(self,X,Y,n):
+    def SCR_matrix(self,X,Y,n):
         """
-        Retorna SCR
+        Returns SCR
 
-        PARAMETROS
+        Parameters
         ----------
-        :X: Esta es la matriz de regresores
-        :Y: Esta es la matriz de la respuesta observada
-        :n: Es el numero de datos
+        X : numpy.matrix 
+            This is the regressor matrix
+        
+        Y : numpy.matrix 
+            This is the matrix of the observed response
+        
+        n : int
+            It is the number of data
+        
         """
-        import numpy as np
+        
         XX = X*X.transpose()
         inverseXX = np.linalg.inv(XX)
 
-        #definimos ones
+        #we define ones
         ones = np.ones(n)
         ones_matrix = np.matrix([ones])
-        oneone =ones_matrix*(ones_matrix.transpose())
+        oneone = ones_matrix*(ones_matrix.transpose())
         ones_final = ones_matrix.transpose() * np.linalg.inv(oneone) * ones_matrix
 
         SCR = (Y * (((X.transpose())*inverseXX*X) - ones_final) * Y.transpose()).item(0)
         return SCR
     
-    def STCC_matricial(self,Y,n):
+    def STCC_matrix(self,Y,n):
         """
-        Retorna STCC
+        Returns STCC
 
-        PARAMETROS
+        Parameters
         ---------------------------------
-        :X: Esta es la matriz de regresores
-        :n: Es el numero de datos
+        X : numpy.matrix  
+            This is the regressor matrix
+        
+        n : int
+            It is the number of data
+        
         """
-        import numpy as np
+        
         ones = np.ones(n)
         ones_matrix = np.matrix([ones])
-        oneone =ones_matrix*(ones_matrix.transpose())
+        oneone = ones_matrix*(ones_matrix.transpose())
         ones_final = ones_matrix.transpose() * np.linalg.inv(oneone) * ones_matrix
 
         STCC = (Y *(np.identity(n) - ones_final)*Y.transpose()).item(0)
         return STCC
    
-    def prediccion_(self,x_pred):
+    def prediction_(self,x_pred):
         """ 
-        Predice un valor segun los valores de los regresores ingresados
+        Predict a value according to the values of the regressors entered
         
-        PARAMETROS
+        Parameters
         ----------
-        :x_pred: Es una lista con los valores de x1,x2,..etc para hacer la estimacion de y 
+        x_pred : list
+            It is a list with the values of x1, x2, ...  to make the estimation of y
         
-        RETORNO
+        Returns
         -------
-        (numpy.float64) Retorna un valor de y estimado
+        np.sum(y_pred) : numpy.float64 
+            Returns an estimated y value
         
         """
-        import numpy as np
-        largo = len(self.coef)
+        
+        length = len(self.coef)
         x_pred.insert(0,1.0)
-        y_pred = [self.coef[i]*x_pred[i] for i in range(largo)]
+        y_pred = [self.coef[i]*x_pred[i] for i in range(length)]
         return np.sum(y_pred)
         
 
